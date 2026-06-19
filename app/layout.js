31a2d2e972ee49b1eed53e68d48c1845601892b0
Paloma Bessa <paloma.pbessa@gmail.com>
2026-06-19 19:10:07 -0300

    Atualiza Google Analytics

--- app/layout.js
+++ app/layout.js
@@ -62,29 +62,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-
-       <head>
-        {GA_ID && (
-          <>
-            <Script
-              strategy="afterInteractive"
-              src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-            />
-            <Script
-              id="gtag-init"
-              strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `
+      {GA_ID && (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
                   window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
                   gtag('config', '${GA_ID}', { page_path: window.location.pathname });
                 `,
-              }}
-            />
-          </>
-        )}
-      </head>
+            }}
+          />
+        </>
+      )}
 
       <body
         className={`
