c764a363bb78a9420a9d428b1ec6664e177a3690
Paloma Bessa <paloma.pbessa@gmail.com>
2026-06-22 15:13:15 -0300

    Sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -2,9 +2,36 @@ export default function sitemap() {
   return [
     {
       url: "https://pbessa.com.br",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
     },
     {
       url: "https://pbessa.com.br/sobre",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
     },
   ];
 }
+
+{
+  url: "https://pbessa.com.br/cases/busca-claro-tv",
+  lastModified: new Date(),
+  changeFrequency: "yearly",
+  priority: 0.7,
+},
+
+{
+  url: "https://pbessa.com.br/cases/landing-claro-tv",
+  lastModified: new Date(),
+  changeFrequency: "yearly",
+  priority: 0.7,
+},
+
+{
+  url: "https://pbessa.com.br/cases/nunt",
+  lastModified: new Date(),
+  changeFrequency: "yearly",
+  priority: 0.7,
+},
